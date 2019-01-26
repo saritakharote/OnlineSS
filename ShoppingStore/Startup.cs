4728813ac60b4f49daaacbe3c8d7b8f8b5e2812a
kharote.sarita gmail.com <kharote.sarita@gmail.com>
2019-01-26 14:15:41 +0545

    Trial

---
+++ ShoppingStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoppingStore.Startup))]
+namespace ShoppingStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
