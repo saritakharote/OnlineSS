7020b1250babab0903af4c1e89c2185bb9009005
kharote.sarita gmail.com <kharote.sarita@gmail.com>
2019-01-28 16:40:06 +0545

    halfdone product

---
+++ ShoppingStore.Services/ProductService.cs
@@ -0,0 +1,68 @@
+﻿using ShoppingStore.Database;
+using ShoppingStore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingStore.Services
+{
+    public class ProductService
+    {
+        //edit categoroy
+        public Product GetProduct(int ID)
+        {
+            using (var context = new SSContext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+
+        //add product
+        public List<Product> GetProducts()
+        {
+            using (var context = new SSContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        //create
+        public void SaveProduct(Product product)
+        {
+            using (var context = new SSContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+
+        //update
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new SSContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+
+            }
+        }
+
+        //delete
+
+
+        public void DeleteProduct(Product product)
+        {
+            using (var context = new SSContext())
+            {
+                //product = context.Products.Find(ID);
+                //context.Entry(product).State = System.Data.Entity.EntityState.Deleted;
+                //OR
+                context.Products.Remove(product);
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
---
+++ ShoppingStore/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using ShoppingStore.Services;
+using System.Web.Mvc;
+using ShoppingStore.Entities;
+
+
+namespace ShoppingStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductService prodserv = new ProductService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductTable(string search)
+        {
+            var products = prodserv.GetProducts();
+            return PartialView(products);
+        }
+
+        //Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            prodserv.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+    }
+}
